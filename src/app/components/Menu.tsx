e11b325b7174990f207f057d8840d1c2c054244e
fruitsPunchSamurai-C <108081381+fruitsPunchSamurai-C@users.noreply.github.com>
2023-11-17 02:48:11 +0100

    deploy test

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-export default function Menu({title}) {
+export default function Menu() {
     return (
         <ul className="menu rounded-box bg-white mr-5 ml-5 border h-70">
             <li className="flex text-sm mb-3">
-                {title}
             </li>
         </ul>
     )
