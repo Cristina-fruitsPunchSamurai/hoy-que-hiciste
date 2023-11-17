59f8fb2ab9ab00a2cfd755e76546b29f4b80cac3
fruitsPunchSamurai-C <108081381+fruitsPunchSamurai-C@users.noreply.github.com>
2023-11-17 02:52:03 +0100

    deploy test

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+// import React from 'react'
 
-export default function Menu() {
-    return (
-        <ul className="menu rounded-box bg-white mr-5 ml-5 border h-70">
-            <li className="flex text-sm mb-3">
-            </li>
-        </ul>
-    )
-}
+// export default function Menu() {
+//     return (
+//         <ul className="menu rounded-box bg-white mr-5 ml-5 border h-70">
+//             <li className="flex text-sm mb-3">
+//             </li>
+//         </ul>
+//     )
+// }
